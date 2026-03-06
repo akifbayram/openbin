a13755dc3177d6f2c9e9094214175a7bca284a41
Mehmet Bayram <akifbayram@gmail.com>
2026-03-06 10:11:02 -0500

    feat: add CSS composition classes for repeated Tailwind patterns

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import './styles/compositions.css';
 
 // biome-ignore lint/style/noNonNullAssertion: standard React root element
 ReactDOM.createRoot(document.getElementById('root')!).render(
